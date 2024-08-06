8fb65d4fc57d701cc3401568e8601488be2fff57
Syedsuhaib1993 <syedsuhaibhussani@gmail.com>
2024-08-06 19:55:47 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,108 @@
+// <<<<<<<<<<<<< CHAPTER 1 ALERT >>>>>>>>>>>>
+// <<<<<<<<<QUESTION 1>>>>>>>>>>
+alert("Hi")
+
+// <<<<<<<<<QUESTION 2>>>>>>>>>>
+alert("Error! Please enter a valid password")
+
+// <<<<<<<<<QUESTION 3>>>>>>>>>>
+alert("Welcome to js land.. Happy coding")
+
+// <<<<<<<<<QUESTION 4>>>>>>>>>>
+alert("Welcome to js land")
+alert("Happy coding")
+
+// <<<<<<<<<QUESTION 5>>>>>>>>>>
+// <<<<<<<<<CODE IN SONSOLE>>>>>>>>>>
+
+// <<<<<<<<<QUESTION 6>>>>>>>>>>
+// <<<<<<<<<USING ALERT IN THE PROJECT>>>>>>>>>>
+
+// <<<<<<<<<QUESTION 7>>>>>>>>>>
+// <<<<<<<<<USING Script in our HTML page>>>>>>>>>>
+
+
+// <<<<<<<<<<<<< CHAPTER 2 VARIABLES FOR STRINGS >>>>>>>>>>>>
+// <<<<<<<<<QUESTION 1>>>>>>>>>>
+var username ;
+
+// <<<<<<<<<QUESTION 2>>>>>>>>>>
+var myname="M.Suhaib";
+
+// <<<<<<<<<QUESTION 3>>>>>>>>>>
+var message="HELLO WORLD" ;
+alert(message);
+
+// <<<<<<<<<QUESTION 4>>>>>>>>>>
+var na = "Ali"
+var age = "25 years"
+var course = "Web development and Mobile app"
+alert(na);
+alert(age);
+alert(course);
+
+// <<<<<<<<<QUESTION 5>>>>>>>>>>
+var p = "pizza"
+alert(p);
+
+// <<<<<<<<<QUESTION 6>>>>>>>>>>
+var email = "Syedsuhaibhussani@gmail.com"
+alert("This is my email" + email + ".")
+
+// <<<<<<<<<QUESTION 7>>>>>>>>>>
+var book = "A Smart way to learn javascript";
+alert("I am trying to learn from the book" + book + ".");
+
+// <<<<<<<<<QUESTION 8>>>>>>>>>>
+console.log("Yah! I am writing JavaScript through HTML content");
+
+// <<<<<<<<<QUESTION 9>>>>>>>>>>
+var end = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+alert(end);
+
+
+// <<<<<<<<<<<<< CHAPTER 3 VARIABLES FOR NUMBERS >>>>>>>>>>>>
+// <<<<<<<<<QUESTION 1>>>>>>>>>>
+alert("I am" + age + "years old")
+
+// <<<<<<<<<QUESTION 2>>>>>>>>>>
+var visit = 0;
+++visit
+alert("you have visited this site" + " " + ++visit + " " + "times" )
+
+// <<<<<<<<<QUESTION 3>>>>>>>>>>
+var birth_year = "My Birthyear is 1993"
+console.log(birth_year)
+
+// <<<<<<<<<QUESTION 4>>>>>>>>>>
+// <<<<<<<<<<<<< not attempt>>>>>>>>>>>>>
+
+
+
+// <<<<<<<<<<<<< CHAPTER 4 LEGAL & ILLEGAL >>>>>>>>>>>>
+// <<<<<<<<<QUESTION 1>>>>>>>>>>
+var a = 5, b = 10 , c = 15 ;
+
+// <<<<<<<<<QUESTION 2>>>>>>>>>>
+// <<<<<<<<<LEGAL VARIABLES>>>>>>>>>>
+var a = "Khan";
+var $me = 10;
+var user_age = 20;
+var MyContact = 123;
+var HARRY = 100;
+
+// <<<<<<<<<ILLEGAL VARIABLES>>>>>>>>>>
+// var 1suhaib = wrong;
+// var my info = wrong;
+// var @email = wrong;
+// var let = wrong;
+// var suh@ib = wrong;
+
+// <<<<<<<<<QUESTION 3>>>>>>>>>>
+// <<<<<<<<<DONE IN HTML>>>>>>>>>>
+
+
+
+// <<<<<<<<<<<<< CHAPTER 5 MATH EXPRESSIONS >>>>>>>>>>>>
+// <<<<<<<<<QUESTION 1>>>>>>>>>>
+
